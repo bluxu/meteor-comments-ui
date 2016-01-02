6ec5ab82d099d3c4243e1f8d506681651a4fc2f9
Edward Hughes <e.f.hughes@qmul.ac.uk>
2016-01-02 13:22:34 +0000

    Minor mods

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'efhughes:comments-ui',
   summary: 'Simple templates for disqus-like comment functionality. Modified for collabor8.',
-  version: '0.3.1',
+  version: '0.3.2',
   git: ''
 });
 
